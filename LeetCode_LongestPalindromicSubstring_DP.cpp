ce98f56231fbed64400700be0780ea9a03bb7d3c
Chiwan Song <scw@Chiwanui-MacBookPro.local>
2019-09-19 16:42:14 +0900

    LeetCode Longest Palindromic Substring DP ver.

---
+++ LeetCode_LongestPalindromicSubstring_DP.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string answer;
+        if(s.length() == 0)
+        {
+            return answer;
+        }
+        answer.push_back(s[0]);
+        for(int i = 0; i < s.length(); i++)
+        {
+            //odd
+            int l = i - 1;
+            int r = i + 1;
+            int lf = i, rf = i;
+            while(l >= 0 && r < s.length())
+            {
+                if(s[l] == s[r])
+                {
+                    lf = l;
+                    rf = r;
+                }
+                else
+                {
+                    break;
+                }
+                l--;
+                r++;
+            }
+            if(rf-lf+1 > answer.length())
+            {
+                answer = s.substr(lf, rf-lf + 1);
+            }
+            //even
+            if(s[i] == s[i + 1])
+            {
+                l = i - 1;
+                r = i + 2;
+                lf = i;
+                rf = i + 1;
+                while(l >= 0 && r < s.length())
+                {
+                    if(s[l] == s[r])
+                    {
+                        lf = l;
+                        rf = r;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    l--;
+                    r++;
+                }
+                if(rf-lf + 1 > answer.length())
+                {
+                    answer = s.substr(lf, rf-lf + 1);
+                }
+            }
+        }
+        return answer;
+    }
+};
