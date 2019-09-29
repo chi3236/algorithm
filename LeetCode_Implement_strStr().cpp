ef2169f164da2b12eb0914c1765629f2162cf9ae
Chiwan Song <chi3236@gmail.com>
2019-09-30 01:10:46 +0900

    LeetCode Implement strStr()

---
+++ LeetCode_Implement_strStr().cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+        {
+            return 0;
+        }
+        if(haystack.empty() || haystack.length() < needle.length())
+        {
+            return -1;
+        }
+        for(int i = 0; i <= haystack.length() - needle.length(); i++)
+        {
+            if(haystack.substr(i, needle.length()) == needle)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
