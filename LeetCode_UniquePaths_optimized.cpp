bcc5b82380f5f8d1a9ba49792d58a162c1b457d3
Chiwan Song <chi3236@gmail.com>
2019-11-03 03:34:47 +0900

    Modify Unique Path Optimized

--- LeetCode_UniquePaths_optimized.cpp
+++ LeetCode_UniquePaths_optimized.cpp
@@ -5,17 +5,14 @@ public:
         {
             return 0;
         }
-        vector<int> prev(n, 1);
-        vector<int> curr(n, 0);
-        curr[0] = 1;
+        vector<int> curr(n, 1);
         for(int i = 1; i < m; i++)
         {
             for(int j = 1; j < n; j++)
             {
-                curr[j] = prev[j] + curr[j-1];
+                curr[j] += curr[j-1];
             }
-            swap(prev, curr);
         }
-        return prev[n-1];
+        return curr[n-1];
     }
 };
