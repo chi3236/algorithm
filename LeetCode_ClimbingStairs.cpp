937fca2a085408e9d81861ded0882d9cb3b866ad
Chiwan Song <chi3236@gmail.com>
2019-11-03 23:03:44 +0900

    LeetCode modify Climbing Stairs

--- LeetCode_ClimbingStairs.cpp
+++ LeetCode_ClimbingStairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int memo[n+1] = {0,};
-        memo[0] = 1;
-        memo[1] = 1;
-        for(int i = 2; i < n+1; i++)
+        int prev = 1;
+        int curr = 1;
+        for(int i = 2; i <= n; i++)
         {
-            memo[i] = memo[i-1] + memo[i-2];
+            int next = curr + prev;
+            prev = curr;
+            curr = next;
         }
-        return memo[n];
+        return curr;
     }
 };
