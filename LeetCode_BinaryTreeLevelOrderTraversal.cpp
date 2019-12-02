4dc5e58f7b033c5cfd90e95b7723eefc03c48228
Chiwan Song <scw@Chiwanui-MacBookPro.local>
2019-12-02 18:27:14 +0900

    modify code

--- LeetCode_BinaryTreeLevelOrderTraversal.cpp
+++ LeetCode_BinaryTreeLevelOrderTraversal.cpp
@@ -1,8 +1,43 @@
-//
-//  LeetCode_BinaryTreeLevelOrderTraversal.cpp
-//  
-//
-//  Created by Chiwan Song on 2019/12/02.
-//
-
-#include "LeetCode_BinaryTreeLevelOrderTraversal.hpp"
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> answer;
+        queue<TreeNode*> q;
+        if(root)
+        {
+            q.push(root);
+        }
+        while(!q.empty())
+        {
+            vector<int> currLev;
+            for(int i = q.size(); i > 0; i--)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                currLev.push_back(curr -> val);
+                if(curr -> left)
+                {
+                    q.push(curr -> left);
+                }
+                if(curr -> right)
+                {
+                    q.push(curr -> right);
+                }
+            }
+            if(!currLev.empty())
+            {
+                answer.push_back(currLev);
+            }
+        }
+        return answer;
+    }
+};
